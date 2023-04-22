64b13986a15404bf1cd79e5568bf076c349c464e
valuecoding <131456030+valuecoding@users.noreply.github.com>
2023-04-22 06:15:01 +0200

    Create MittelwertOhneFunktion.cpp
    Mittelwert bestimmen OHNE Funktion.

---
+++ MittelwertOhneFunktion.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+/* Mittelwert OHNE Funktion */
+
+int main()
+{
+    
+    int first = 20;
+    int second = 100;
+    int average = (first + second / 2);
+    
+    cout << average;
+    
+    first = 100;
+    second = 200;
+    average = (first + second / 2);
+    
+    cout << average;
+    
+    
+}// main end
+
